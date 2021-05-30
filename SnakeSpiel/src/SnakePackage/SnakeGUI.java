4848558f32eb294e878b5774d152ec6352b525be
mirsadavdic <mirsadavdic@mirsads-air.fritz.box>
2021-05-30 17:41:02 +0200

    Initial commit of project.

---
+++ SnakeSpiel/src/SnakePackage/SnakeGUI.java
@@ -0,0 +1,10 @@
+package SnakePackage;
+
+public class SnakeGUI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SnakeSpiel/src/SnakePackage/SnakePanel.java
@@ -0,0 +1,5 @@
+package SnakePackage;
+
+public class SnakePanel {
+
+}
